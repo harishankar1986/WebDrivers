3873817eefa732ba0e22fc2182e8abd08bc174cb
Hari <harishankarvanka@gmail.com>
2020-09-26 12:33:39 +0530

    First commit

---
+++ webDrivers/src/test/java/Selenium/LaunchChrome.java
@@ -0,0 +1,15 @@
+package Selenium;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LaunchChrome {
+
+	public static void main(String[] args) {
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\LENOVO\\Desktop\\Testing master\\chromedriver.exe");
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("https://www.facebook.com/");
+
+	}
+
+}
